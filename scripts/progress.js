cc4760554ea8941257d2ee7d739614346358ab37
yardr <webtraveller22@gmail.com>
2017-03-13 11:34:03 +0000

    changes

---
+++ scripts/progress.js
@@ -0,0 +1,20 @@
+function move(item, length) {
+  var elem = document.getElementById(item);   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= length) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+move('myBar', 90);
+move('myBar2', 90);
+move('myBar3', 50);
+move('myBar4', 60);
+move('myBar5', 60);
+move('myBar6', 50);
+move('myBar7', 50);
