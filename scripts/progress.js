5a40905aac7a3c0833f57114ae384f68319d872d
yardr <webtraveller22@gmail.com>
2017-04-19 10:07:34 +0000

    jquery script modified

--- scripts/progress.js
+++ scripts/progress.js
@@ -1,4 +1,4 @@
-function move(item, length) {
+ function move(item, length) {
   var elem = document.getElementById(item);   
   var width = 1;
   var id = setInterval(frame, 10);
@@ -11,10 +11,16 @@ function move(item, length) {
     }
   }
 }
+$(function() {
+  $('.skill-wrapper').waypoint(function() {
 move('myBar', 90);
 move('myBar2', 90);
 move('myBar3', 50);
 move('myBar4', 60);
-move('myBar5', 50);
+move('myBar5', 60);
 move('myBar6', 50);
-move('myBar7', 40);
+move('myBar7', 50);
+      }, {
+           offset: '100%'
+         });
+});
